525f20257666fd047ff44aacc02fff408789620a
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-06-13 18:27:06 +0300

    Solve the task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""This module provides an integer addition function."""
+
+
 def add_integer(a, b=98):
+    """Add two integers or floats after casting them to integers."""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
