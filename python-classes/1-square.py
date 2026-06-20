40126d47be61cc4efbf1d196fe3aacf7cdef7124
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-06-20 09:39:31 +0300

    Solve the first task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Defines a Square class."""
+
 
 class Square:
+    """A class that defines a square."""
+
     def __init__(self, size):
+        """Initialize a square with size.
+
+        Args:
+            size: The size of the square.
+        """
         self.__size = size
