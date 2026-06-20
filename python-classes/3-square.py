0174b4122e701aad5901e149855f5d3d68b62408
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-06-20 16:52:08 +0300

    Solve the first task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
+"""Defines a Square class."""
 
-"""defines a square class."""
 
-class square:
-    
-    """a class that defines a square"""
+class Square:
+    """A class that defines a square."""
 
     def __init__(self, size=0):
-
         """Initialize a square with validated size.
 
         Args:
@@ -21,9 +19,9 @@ class square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-    
+
         self.__size = size
 
     def area(self):
-        """return the current square area."""
+        """Return the current square area."""
         return self.__size ** 2
