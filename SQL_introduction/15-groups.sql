353af971c3ede869ae94cb43aca2be56a01eef0a
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-07-24 16:32:28 +0300

    slove the Task 15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- Combine the row that have the same score 
 -- By use GROUP BY Command
-SELECT socre, COUNT(*) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score 
 ORDER BY number DESC;
