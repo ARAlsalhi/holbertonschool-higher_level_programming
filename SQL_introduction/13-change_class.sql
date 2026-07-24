6ce6f714c643ca7f99219eacbef4c4d0ea1efaba
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-07-24 15:12:45 +0300

    slove the Task 13

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,4 +1,4 @@
 -- Filter the score so we delete any score less than or equales 5
 -- DELETE is remove the row so we DONT put column name after DELETE 
-DELETE FROM seconde_table
+DELETE FROM second_table
 WHERE score <= 5;
