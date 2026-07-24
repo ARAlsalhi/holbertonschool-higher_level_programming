e59eb350f46e6a68ebb63b9924f1602af5900c3a
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-07-24 10:56:21 +0300

    slove the Task 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
 -- First Table
-CREATE TABLE IF NOT EXISTS _in_hbtn_0c_0 (
-
+CREATE TABLE IF NOT EXISTS first_table (
     id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     constraint pk_id PRIMARY KEY (id)
