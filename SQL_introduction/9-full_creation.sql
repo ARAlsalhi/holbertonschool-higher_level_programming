bb566ba79fc5b416d432af571b9069711cb297a1
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-07-24 14:41:23 +0300

    Fix the error in the Task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -3,14 +3,15 @@
 CREATE TABLE IF NOT EXISTS second_table (
     id INT NOT NULL, 
     name VARCHAR(256) NOT NULL, 
-    score INT NOT NULL
+    score INT NOT NULL,
     constraint pk_id PRiMARY KEY (id)
 );
 
 -- Add new Row 
 
-INSERT INTO second_table (id, name, score)
-    VALUES (1, 'John', 10),
-    VALUES (2, 'Alex', 3),
-    VALUES (3, 'Bob', 14),
-    VALUES (4, 'George', 8);
+INSERT INTO second_table (id, name, score) VALUES
+
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
