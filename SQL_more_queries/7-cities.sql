235b67c72fa5fd253d4a2e67e7ef19bac2152b44
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-07-25 18:21:39 +0300

    slove the Task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- AS I DO IN THE TASK 6
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL UNIQUE AUTO_INCREMENT, 
+    state_id INT NOT NULL ,
+    name VARCHAR(256),
+    PRIMARY KEY (id),
+    FOREIGN KEY (states_id) REFERENCES states(id)
+);
