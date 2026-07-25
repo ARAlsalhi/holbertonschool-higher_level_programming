84f6b8d4a1c2c126088fb7caf8fa2b057044b7f7
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-07-25 19:45:44 +0300

    slove the Task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,6 @@
 -- Genre ID by show
-SELECT 
-    TS.title, 
-    TSG.genre_id
+SELECT TS.title, TSG.genre_id
 FROM tv_shows AS TS 
 INNER JOIN tv_show_genres AS TSG 
-ON TS.id = TSG.genre_id 
+ON TS.id = TSG.show_id 
 ORDER BY TS.title ASC, TSG.genre_id ASC;
