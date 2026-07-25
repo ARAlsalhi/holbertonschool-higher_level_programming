03b9814b3c55be596a175c04449b99771cbb92a8
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-07-25 17:26:05 +0300

    slove the Task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- THIS COMMAND TO DISPLAY THE PRIVILEAGE
-SHOW GRANTS FOR 'user_0d_1@localhost';
-SHOW GRANTS FOR 'user_0d_2@localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
