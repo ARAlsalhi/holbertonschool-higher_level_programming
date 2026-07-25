d92b773b2f0a54626bdf767d86fd6d35907fe233
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-07-25 19:59:54 +0300

    slove the Task 11

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,6 +3,6 @@ SELECT
     tv_shows.title, 
     tv_show_genres.genre_id
 FROM tv_shows
-LEFT join tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_show.id = tv_show_genres.show_id
 ORDER BY  tv_shows.title ASC , tv_show_genres.genre_id ASC; 
