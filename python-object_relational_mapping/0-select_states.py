9274f1f9f793baea8ec18b45152d3e6f2ad1c20e
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-07-25 22:24:16 +0300

    slove the Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,15 +5,12 @@
 Lists all states from the database passed as an argument.
 Results are sorted by states.id in ascending order.
 """
-#!/usr/bin/python3
-"""Lists all states from the given database."""
-
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-    connection = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
@@ -21,16 +18,12 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
 
-    cursor = connection.cursor()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
 
-    cursor.execute(
-        "SELECT * FROM states ORDER BY id ASC"
-    )
-
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
+    for row in rows:
+        print(row)
 
     cursor.close()
-    connection.close()
+    db.close()
