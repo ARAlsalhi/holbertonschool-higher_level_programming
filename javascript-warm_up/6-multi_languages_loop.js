4965c10a496b0c5b4132518180a9efd73846bc5c
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-08-18 15:33:21 +0300

    T6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
-const AR = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const AR = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
 
 for (let i = 0; i < AR.length; i++) {
-    console.log(i);
+  console.log(AR[i]);
 }
