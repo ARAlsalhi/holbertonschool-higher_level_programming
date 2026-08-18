02ad47efd71dcd640d3bc3ed4b1bf2752618c9a3
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-08-18 14:43:24 +0300

    T1,2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,8 +3,8 @@
 const numOfArg = process.argv.length - 2;
 
 if(numOfArg === 0)
-    console.log('No argument');
+    console.log("No argument");
 else if(numOfArg === 1)
-    console.log('Argument found');
+    console.log("Argument found");
 else
-    console.log('Argument found');
+    console.log("Argument found");
