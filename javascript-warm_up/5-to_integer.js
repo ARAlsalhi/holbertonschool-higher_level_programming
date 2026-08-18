bcd0a81f5187386b3e07e83ec0ce6e50af381730
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-08-18 15:21:02 +0300

    T5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const num = parseInt(process.argv[2]);
+const number = parseInt(process.argv[2]);
 
-if (isNaN(num)) {
-    console.log('Not a number');
-}
-else {
-    console.log(`My number: ${num}`);
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${number}`);
 }
