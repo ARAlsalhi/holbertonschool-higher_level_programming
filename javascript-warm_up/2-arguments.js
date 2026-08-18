2bd4b3a1f59fb62a7350ad2fe2789278c8eba915
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-08-18 14:47:36 +0300

    T1,2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-
 const numOfArg = process.argv.length - 2;
-
 if(numOfArg === 0)
-    console.log("No argument");
-else if(numOfArg === 1)
-    console.log("Argument found");
-else
-    console.log("Argument found");
+    console.log('No argument');
+    else if(numOfArg === 1)
+        console.log('Argument found');
+    else
+        console.log('Argument found');
